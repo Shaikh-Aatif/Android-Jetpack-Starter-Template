6dd974b2c7e2c6eeb7635b41b90f6d04d0a5d664
Shaikh-Aatif <aatifshaikh523@gmail.com>
2023-11-24 14:33:10 +0530

    new development branch

--- app/src/main/java/com/example/startertemplate/MainActivity.kt
+++ app/src/main/java/com/example/startertemplate/MainActivity.kt
@@ -19,6 +19,8 @@ class MainActivity : ComponentActivity() {
             StarterTemplateTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
+
+
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
